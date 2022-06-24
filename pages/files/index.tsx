742aa91c7993af38db13f3151c004a204955151d
Obi Fortune <gabrielobi.of@gmail.com>
2022-06-24 20:24:18 +0300

    + Added keys to the file card + Added a delete button

--- pages/files/index.tsx
+++ pages/files/index.tsx
@@ -9,11 +9,13 @@ import {
   Stack,
   Text,
   Title,
+  Tooltip,
   useMantineTheme,
 } from '@mantine/core';
 import { NextLink } from '@mantine/next';
+import axios from 'axios';
 import { useFetchAllFiles } from 'hooks';
-import { AlertCircle, Download } from 'tabler-icons-react';
+import { AlertCircle, Download, X } from 'tabler-icons-react';
 
 const Index = () => {
   const { data, error, loading } = useFetchAllFiles({});
@@ -40,12 +42,20 @@ const Index = () => {
 
   const { files } = data;
 
+  const deleteFileHandler = async (fileId: string) => {
+    const res = await axios.delete(`/api/file/${fileId}`);
+
+    if (res.status === 200) {
+      window.location.reload();
+    }
+  };
+
   return (
     <Container className="h-100">
       {files.length > 0 ? (
         <Stack>
           {files.map(file => (
-            <Card shadow="sm" p="lg">
+            <Card shadow="sm" p="lg" key={String(file._id)}>
               <Group
                 position="apart"
                 style={{ marginBottom: 5, marginTop: theme.spacing.sm }}
@@ -53,18 +63,34 @@ const Index = () => {
                 <Title order={3} style={{ textDecoration: 'underline' }}>
                   {file.name}
                 </Title>
-                <Button
-                  component={NextLink}
-                  variant="light"
-                  color="blue"
-                  style={{ marginTop: 14 }}
-                  download
-                  rel="noopener noreferrer"
-                  target="_blank"
-                  href={`api/download?filename=${file.url}`}
-                >
-                  <Download />
-                </Button>
+
+                <Group>
+                  <Tooltip label="Download" withArrow>
+                    <Button
+                      component={NextLink}
+                      variant="light"
+                      color="blue"
+                      style={{ marginTop: 14 }}
+                      download
+                      rel="noopener noreferrer"
+                      target="_blank"
+                      href={`api/download?filename=${file.url}`}
+                    >
+                      <Download />
+                    </Button>
+                  </Tooltip>
+
+                  <Tooltip label="Delete file" withArrow color="red">
+                    <Button
+                      variant="light"
+                      color="red"
+                      style={{ marginTop: 14 }}
+                      onClick={() => deleteFileHandler(String(file._id))}
+                    >
+                      <X />
+                    </Button>
+                  </Tooltip>
+                </Group>
               </Group>
 
               <Text
