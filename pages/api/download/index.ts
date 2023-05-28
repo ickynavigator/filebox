0ab26f374437e10fbbcc0c2994c5095c5424f172
Obi Fortune <gabrielobi.of@gmail.com>
2023-05-28 18:49:56 +0100

    added method switch case to api route

--- pages/api/download/index.ts
+++ pages/api/download/index.ts
@@ -5,23 +5,32 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
-  // path to file
-  const filePath = req.query.filename as string;
+  switch (req.method) {
+    case 'GET': {
+      // path to file
+      const filePath = req.query.filename as string;
 
-  // filename only
-  const fileName = filePath.substring(filePath.lastIndexOf('/') + 1);
+      // filename only
+      const fileName = filePath.substring(filePath.lastIndexOf('/') + 1);
 
-  // set header
-  res.setHeader('content-disposition', `attachment; filename=${fileName}`);
+      // set header
+      res.setHeader('content-disposition', `attachment; filename=${fileName}`);
 
-  // send request to the original file
-  request
-    .get(filePath) // download original image
-    .on('error', err => {
-      console.error(err);
-      res.writeHead(404, { 'Content-Type': 'text/html' });
-      res.write('<h1>404 not found</h1>');
-      res.end();
-    })
-    .pipe(res); // pipe converted image to HTTP response
+      // send request to the original file
+      return request
+        .get(filePath) // download original image
+        .on('error', err => {
+          console.error(err);
+          res.writeHead(404, { 'Content-Type': 'text/html' });
+          res.write('<h1>404 not found</h1>');
+          res.end();
+        })
+        .pipe(res); // pipe converted image to HTTP response
+    }
+    default: {
+      return res.status(405).json({
+        message: 'Method not allowed',
+      });
+    }
+  }
 }
