3507c66fe15b0a195cebf3c641096f0305ad5d25
Obi Fortune <gabrielobi.of@gmail.com>
2023-07-08 09:14:20 +0100

    update dropzone api

--- components/FileUpload.tsx
+++ components/FileUpload.tsx
@@ -1,94 +1,11 @@
 import { Notifications as message } from '>lib/notifications';
-import {
-  Group,
-  MantineTheme,
-  Space,
-  Text,
-  useMantineTheme,
-} from '@mantine/core';
-import { Dropzone, DropzoneStatus } from '@mantine/dropzone';
+import { Group, Space, Text, useMantineTheme } from '@mantine/core';
+import { Dropzone, FileRejection } from '@mantine/dropzone';
+import { IconPhoto, IconUpload, IconX } from '@tabler/icons-react';
 import React from 'react';
-import { ErrorCode, FileRejection } from 'react-dropzone';
-import { Icon as TablerIcon, Photo, Upload, X } from 'tabler-icons-react';
+import { ErrorCode } from 'react-dropzone';
 
-function getIconColor(status: DropzoneStatus, theme: MantineTheme): string {
-  if (status.accepted) {
-    return theme.colors[theme.primaryColor][
-      theme.colorScheme === 'dark' ? 4 : 6
-    ];
-  }
-
-  if (status.rejected) {
-    return theme.colors.red[theme.colorScheme === 'dark' ? 4 : 6];
-  }
-
-  if (theme.colorScheme === 'dark') {
-    return theme.colors.dark[0];
-  }
-
-  return theme.colors.gray[7];
-
-  // Original ternary converted for eslint whining
-  //
-  // return status.accepted
-  //   ? theme.colors[theme.primaryColor][theme.colorScheme === 'dark' ? 4 : 6]
-  //   : status.rejected
-  //   ? theme.colors.red[theme.colorScheme === 'dark' ? 4 : 6]
-  //   : theme.colorScheme === 'dark'
-  //   ? theme.colors.dark[0]
-  //   : theme.colors.gray[7];
-}
-
-function ImageUploadIcon({
-  status,
-  ...props
-}: React.ComponentProps<TablerIcon> & { status: DropzoneStatus }) {
-  if (status.accepted) {
-    return <Upload {...props} />;
-  }
-
-  if (status.rejected) {
-    return <X {...props} />;
-  }
-
-  return <Photo {...props} />;
-}
-
-function dropzoneChildren(
-  status: DropzoneStatus,
-  theme: MantineTheme,
-  size?: number,
-  child?: React.ReactNode,
-) {
-  const bytesToMegaBytes = (bytes: number) => bytes / 1024 ** 2;
-
-  return (
-    <>
-      <Group
-        position="center"
-        spacing="xl"
-        style={{ minHeight: 220, pointerEvents: 'none' }}
-      >
-        <ImageUploadIcon
-          status={status}
-          style={{ color: getIconColor(status, theme) }}
-          size={80}
-        />
-        <Text size="sm" color="dimmed" inline mt={7}>
-          Attach files as you like
-          {size && `, files should not exceed ${bytesToMegaBytes(size)}mb`}
-        </Text>
-      </Group>
-
-      {child && (
-        <>
-          <Space h="md" />
-          <Group position="center">{child}</Group>
-        </>
-      )}
-    </>
-  );
-}
+const bytesToMegaBytes = (bytes: number) => bytes / 1024 ** 2;
 
 /**
  * Verifies the File
@@ -191,7 +108,47 @@ const Index: React.FC<FileUploadProps> = props => {
       maxSize={MAX_FILE_SIZE}
       accept={FILE_TYPE}
     >
-      {status => dropzoneChildren(status, theme, MAX_FILE_SIZE, child)}
+      <Group
+        position="center"
+        spacing="xl"
+        style={{ minHeight: 220, pointerEvents: 'none' }}
+      >
+        <Dropzone.Accept>
+          <IconUpload
+            size="3.2rem"
+            color={
+              theme.colors[theme.primaryColor][
+                theme.colorScheme === 'dark' ? 4 : 6
+              ]
+            }
+          />
+        </Dropzone.Accept>
+
+        <Dropzone.Reject>
+          <IconX
+            size="3.2rem"
+            color={theme.colors.red[theme.colorScheme === 'dark' ? 4 : 6]}
+          />
+        </Dropzone.Reject>
+
+        <Dropzone.Idle>
+          <IconPhoto size="3.2rem" color={theme.colors.dark[0]} />
+        </Dropzone.Idle>
+
+        <Text size="sm" color="dimmed" inline mt={7}>
+          Attach files as you like
+          {MAX_FILE_SIZE
+            ? `, files should not exceed ${bytesToMegaBytes(MAX_FILE_SIZE)}mb`
+            : null}
+        </Text>
+      </Group>
+
+      {child && (
+        <>
+          <Space h="md" />
+          <Group position="center">{child}</Group>
+        </>
+      )}
     </Dropzone>
   );
 };
