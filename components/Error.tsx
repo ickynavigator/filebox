744c6f0a0566d0431e3a7e56b02664ebbdeff301
Obi Fortune <gabrielobi.of@gmail.com>
2023-10-02 12:28:59 +0100

    add retry logic

--- components/Error.tsx
+++ components/Error.tsx
@@ -1,17 +1,48 @@
-import { Alert, Center, Container } from '@mantine/core';
+import { Alert, Button, Center, Container, Stack } from '@mantine/core';
 import { AlertCircle } from 'tabler-icons-react';
 
 interface Props {
   message?: string;
+  retry?: {
+    show?: boolean;
+    onClick?: () => void;
+    message?: string;
+  };
 }
-const Index: React.FC<Props> = ({ message }) => (
-  <Container>
-    <Center py="md">
-      <Alert icon={<AlertCircle size={16} />} title="Bummer!" color="red">
-        {message ?? 'Sorry but an error occurred.'}
-      </Alert>
-    </Center>
-  </Container>
-);
+const Index: React.FC<Props> = props => {
+  const { message, retry } = props;
+  const handleRetry = () => {
+    if (retry && retry.onClick) {
+      retry.onClick();
+      return;
+    }
+
+    if (typeof window !== 'undefined') {
+      window.location.reload();
+    }
+  };
+  return (
+    <Container>
+      <Center py="md">
+        <Stack>
+          <Alert icon={<AlertCircle size={16} />} title="Bummer!" color="red">
+            {message ?? 'Sorry but an error occurred.'}
+          </Alert>
+
+          {retry?.show && (
+            <Button
+              onClick={handleRetry}
+              variant="outline"
+              color="yellow"
+              leftIcon={<AlertCircle size={16} />}
+            >
+              {retry?.message ?? 'Reload Page'}
+            </Button>
+          )}
+        </Stack>
+      </Center>
+    </Container>
+  );
+};
 
 export default Index;
