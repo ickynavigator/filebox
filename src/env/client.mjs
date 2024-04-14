c1ba7925a4db33534cf7f92c25987bf5a5a4f874
Obi Fortune <gabrielobi.of@gmail.com>
2024-04-14 16:18:42 -0400

    remove env files

--- src/env/client.mjs
+++
@@ -1,9 +0,0 @@
-import { createEnv } from '@t3-oss/env-nextjs';
-import { z } from 'zod';
-
-export const env = createEnv({
-  server: {
-    NEXT_PUBLIC_BUCKET_URL: z.string(),
-  },
-  runtimeEnv: process.env,
-});
