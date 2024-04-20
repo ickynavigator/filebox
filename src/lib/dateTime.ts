adbb37fda5cd84256b953d7244e5222db7e53895
Obi Fortune <gabrielobi.of@gmail.com>
2024-04-20 03:20:40 -0400

    update relative time setup

--- src/lib/dateTime.ts
+++ src/lib/dateTime.ts
@@ -1,6 +1,16 @@
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
+import utc from 'dayjs/plugin/utc';
 
 dayjs.extend(relativeTime);
+dayjs.extend(utc);
 
-export const timeFromNow = (date: string | Date) => dayjs(date).fromNow();
+export const timeFromNow = (date: string | Date | null) => {
+  const dateToParse = dayjs.utc(date);
+
+  const hasDateReached = dayjs.utc().isAfter(dateToParse);
+
+  const today = dayjs.utc().endOf('day');
+
+  return hasDateReached ? today.fromNow() : dateToParse.fromNow();
+};
