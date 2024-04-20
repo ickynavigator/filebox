0acc551e4dbfaee53950588ded2ab0ff00bd5b2f
Obi Fortune <gabrielobi.of@gmail.com>
2024-04-19 23:58:36 -0400

    - update form styling - add the tag input component

--- src/app/files/upload/_form.tsx
+++ src/app/files/upload/_form.tsx
@@ -3,20 +3,25 @@
 import {
   Button,
   Container,
-  Space,
+  Group,
+  Stack,
   Text,
   TextInput,
   Textarea,
 } from '@mantine/core';
 import { useForm, zodResolver } from '@mantine/form';
+import { Tag } from '@prisma/client';
 import { FormEvent, useState } from 'react';
 import { z } from 'zod';
 import { uploadFormData } from '~/actions/aws';
 import { FileUpload, type FileInterface } from '~/components/FileUpload';
+import CustomTagInput from '~/components/customTagInput';
 import { MAX_UPLOAD_FILE_SIZE } from '~/lib/constants';
 import { Notifications } from '~/lib/notifications';
 
-interface Props {}
+interface Props {
+  tags?: Tag[];
+}
 
 const schema = z.object({
   name: z
@@ -29,8 +34,7 @@ const schema = z.object({
 });
 
 export function Form(props: Props) {
-  // eslint-disable-next-line no-empty-pattern
-  const {} = props;
+  const { tags } = props;
 
   const [files, setFiles] = useState<FileInterface[]>([]);
 
@@ -44,7 +48,7 @@ export function Form(props: Props) {
   };
 
   const submitHandler = async (
-    values: typeof form.values,
+    _values: typeof form.values,
     event: FormEvent<HTMLFormElement> | undefined,
   ) => {
     event?.preventDefault();
@@ -58,15 +62,15 @@ export function Form(props: Props) {
     }
 
     try {
-      const formData = new FormData();
-      formData.append('file', fileToUpload.file);
-      formData.append('name', values.name);
-      formData.append('description', values.description);
+      const formData = new FormData(event?.currentTarget);
+
+      formData.set('file', fileToUpload.file);
 
       await uploadFormData(formData);
 
       Notifications.success('File uploaded successfully');
 
+      return;
       form.reset();
       setFiles([]);
     } catch (error) {
@@ -79,43 +83,47 @@ export function Form(props: Props) {
   return (
     <Container py="lg">
       <form onSubmit={form.onSubmit(submitHandler)}>
-        <FileUpload
-          files={files}
-          setFiles={setFiles}
-          onDrop={onDrop}
-          MAX_FILE_SIZE={MAX_UPLOAD_FILE_SIZE}
-          singleFile
-        >
-          <Text size="sm" c="dimmed" inline>
-            Only one file should be uploaded at a time
-          </Text>
-        </FileUpload>
+        <Stack gap="md">
+          <FileUpload
+            files={files}
+            setFiles={setFiles}
+            onDrop={onDrop}
+            MAX_FILE_SIZE={MAX_UPLOAD_FILE_SIZE}
+            singleFile
+          >
+            <Text size="sm" c="dimmed" inline>
+              Only one file should be uploaded at a time
+            </Text>
+          </FileUpload>
 
-        {files[0] && (
-          <>
-            <Space h="md" />
-            <TextInput
-              name="name"
-              placeholder="Enter file name"
-              label="File Name"
-              required
-              {...form.getInputProps('name')}
-            />
-            <Textarea
-              name="description"
-              placeholder="Enter file description"
-              label="File Description"
-              autosize
-              minRows={2}
-              maxRows={4}
-              {...form.getInputProps('description')}
-            />
+          {files[0] ? (
+            <>
+              <TextInput
+                name="name"
+                placeholder="Enter file name"
+                label="File Name"
+                required
+                {...form.getInputProps('name')}
+              />
 
-            <Button type="submit" my="md">
-              Upload File
-            </Button>
-          </>
-        )}
+              <CustomTagInput tags={tags} />
+
+              <Textarea
+                name="description"
+                placeholder="Enter file description"
+                label="File Description"
+                autosize
+                minRows={4}
+                maxRows={6}
+                {...form.getInputProps('description')}
+              />
+
+              <Group justify="flex-end">
+                <Button type="submit">Upload File</Button>
+              </Group>
+            </>
+          ) : null}
+        </Stack>
       </form>
     </Container>
   );
--- src/app/files/upload/page.tsx
+++ src/app/files/upload/page.tsx
@@ -1,12 +1,15 @@
 import type { Metadata } from 'next';
+import { getTagsCached } from '~/actions/tags';
 import { Form } from './_form';
 
 export const metadata: Metadata = {
   title: 'Upload',
 };
 
-function Page() {
-  return <Form />;
+async function Page() {
+  const tags = await getTagsCached();
+
+  return <Form tags={tags} />;
 }
 
 export default Page;
