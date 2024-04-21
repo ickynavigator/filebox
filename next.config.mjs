ae46c56a86e6bad651facebe9c7619f40f9ae19c
Obi Fortune <gabrielobi.of@gmail.com>
2024-04-21 02:06:58 -0400

    - install jiti - optimize package imports

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,15 @@
-import './src/env/index.mjs';
+import { fileURLToPath } from 'node:url';
+import createJiti from 'jiti';
+
+// Import env here to validate during build. Using jiti we can import .ts files :)
+createJiti(fileURLToPath(import.meta.url))('./src/env/index.mjs');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    optimizePackageImports: ['@mantine/core', '@mantine/hooks'],
+  },
 };
 
 export default nextConfig;
